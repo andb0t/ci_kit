35b7befdc42c4aeb341750d18751d506408b7cd0
Andreas Alexander Maier <andb0t@pm.me>
2019-08-02 18:36:48 +0200

    Update setup_ci script

--- setup_ci.sh
+++ setup_ci.sh
@@ -15,23 +15,33 @@ REP_ROOT=$(git rev-parse --show-toplevel)
 KIT_ROOT="$( cd "$(dirname "$0")" || exit; pwd -P )"
 
 # find out if it is submodule
-REPO_DIFF="$(diff -q "$REP_ROOT" "$KIT_ROOT")"
+REPO_DIFF="$(diff -q "$REP_ROOT" "$KIT_ROOT/..")"
 if [[ -z "$REPO_DIFF" ]]; then
   IS_SUBMODULE="Y"
 else
   IS_SUBMODULE="N"
 fi
 
-echo "REP_ROOT:     $REP_ROOT"
-echo "KIT_ROOT:     $KIT_ROOT"
-echo "IS_SUBMODULE: $IS_SUBMODULE"
-
 
 # copy pipeline files
 if [[ "$GIT_PROVIDER" == "GITLAB" ]]; then
   yml_file="gitlab-ci.yml"
   cp "$KIT_ROOT/$yml_file" "$REP_ROOT/.$yml_file"
-  cp -r "$KIT_ROOT/ci_tests" "$REP_ROOT/.gitlab-ci.d"
 else
   echo "Not implemented, yet! Please contribute!"
+fi
+
+# fix path placeholders in yml file
+if [[ "$IS_SUBMODULE" == "Y" ]]; then
+  SCRIPT_PATH="./ci_kit/"
+else
+  SCRIPT_PATH="."
+fi
+sed -i "s#CI_KIT_PATH#$SCRIPT_PATH#g" ".$yml_file"
+
+# copy scipts
+if [[ "$IS_SUBMODULE" == "N" ]]; then
+  # copy script files
+  cp -r "$KIT_ROOT/githooks" "$REP_ROOT/.githooks"
+  cp -r "$KIT_ROOT/git-ci.d" "$REP_ROOT/.git-ci.d"
 fi
